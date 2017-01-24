853f3c37bbe394d0ea88c58de5cb4e2618c7eb3c
seanng <shonum@gmail.com>
2017-01-25 00:49:00 +0800

    Create DB controllers

--- server/db/controllers.js
+++ server/db/controllers.js
@@ -1,5 +1,18 @@
 const { UniqueObject, StateChange } = require('./config.js')
 
+const findObject = (objectType, objectNumber, cb) => {
+  UniqueObject.findOne({ where: { objectType, objectNumber } })
+  .then(cb).catch(cb)
+}
+
+const retrieveAndUpdateObject = (type, id, timestamp, changes, cb) => {
+  findObject(type, id, obj => {
+    if (!obj) console.log('no obj', obj);
+    console.log('found obj', obj);
+    cb()
+  })
+}
+
 const postHandler = (json, callback) => {
   json.data.forEach( row => {
     const type = row.object_type,
@@ -8,10 +21,8 @@ const postHandler = (json, callback) => {
        changes = row.object_changes;
 
     if (type !== '' && id !== '') {
-      retrieveAndUpdateObject(type, id, timestamp, changes);
+      retrieveAndUpdateObject(type, id, timestamp, changes, callback);
     }
-
-
   })
 }
 
